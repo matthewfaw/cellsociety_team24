e67944697f49086bf5328233a2c9b4ea401c278d
Matthew Faw <mcf28@duke.edu>
2016-09-24 22:50:32 -0400

    Refactored some of the CellStyleGuideFactory code to 1. use resource bundles and 2. be more readable

--- src/xml/view_factories/CellStyleGuideFactory.java
+++ src/xml/view_factories/CellStyleGuideFactory.java
@@ -1,23 +1,31 @@
 package xml.view_factories;
 
-import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import views.styles.CellStyleGuide;
 import xml.XMLReader;
 
+/**
+ * A factory to pull cell styling info from XML
+ * @author matthewfaw
+ *
+ */
 public class CellStyleGuideFactory {
+	private static final String RESOURCE_PATH = "resources/CellStyleguide";
 	private XMLReader fXmlReader;
-	NodeList fStateXMLNodes;
-	CellStyleGuide fCellStyleGuide;
+	private NodeList fStateXMLNodes;
+	private CellStyleGuide fCellStyleGuide;
+	private ResourceBundle fCellStyleGuideRB;
 	
 	public CellStyleGuideFactory(String fXmlFilename)
 	{
+		fCellStyleGuideRB = ResourceBundle.getBundle(RESOURCE_PATH);
+
 		fXmlReader = new XMLReader(fXmlFilename);
-		fStateXMLNodes = fXmlReader.findElements("state");
+		fStateXMLNodes = fXmlReader.findElements(getResource("StateElement"));
 		fCellStyleGuide = new CellStyleGuide();
 	}
 	
@@ -28,17 +36,38 @@ public class CellStyleGuideFactory {
 	public CellStyleGuide createStyleGuide()
 	{
 		for (int i=0; i<fStateXMLNodes.getLength(); ++i) {
-			Element stateNode = (Element) fStateXMLNodes.item(i);
-			int stateIndex = Integer.parseInt(stateNode.getAttribute("id"));
+			Element stateNode = getStateElement(i);
 			
-			Element viewElement = (Element) fXmlReader.findElements(stateNode, "viewInfo").item(0);
-			String color = fXmlReader.getTextValue(viewElement, "color");
-			String name = fXmlReader.getTextValue(viewElement, "name");
-			
-			fCellStyleGuide.setColor(stateIndex, color);
-			fCellStyleGuide.setName(stateIndex, name);
+			appendStyleGuideInfo(stateNode);
 		}
 		
 		return fCellStyleGuide;
 	}
+	
+	private Element getStateElement(int aIndex)
+	{
+		return (Element) fStateXMLNodes.item(aIndex);
+	}
+	
+	private void appendStyleGuideInfo(Element aStateNode)
+	{
+		int stateIndex = Integer.parseInt(aStateNode.getAttribute(getResource("StateId")));
+
+		Element viewElement = fXmlReader.findFirstChildElement(aStateNode, getResource("ViewInfoTag"));
+		String color = fXmlReader.getTextValue(viewElement, getResource("ColorTag"));
+		String name = fXmlReader.getTextValue(viewElement, getResource("NameTag"));
+
+		fCellStyleGuide.setColor(stateIndex, color);
+		fCellStyleGuide.setName(stateIndex, name);
+	}
+	
+	private String getResource(String aResourceToRetrieve)
+	{
+		return fCellStyleGuideRB.getString(aResourceToRetrieve);
+	}
+//	public static void main(String[] args)
+//	{
+//		CellStyleGuideFactory f = new CellStyleGuideFactory("state_config/test_states.xml");
+//		f.createStyleGuide();
+//	}
 }
