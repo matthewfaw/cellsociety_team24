281a1e6ab8c63c14ab46ed94564892141544b74c
Weston Carvalho <wac12@duke.edu>
2016-09-25 13:47:58 -0400

    Begins to make CellState stored in Map. Rules not updated yet.

--- src/models/grid/Cell.java
+++ src/models/grid/Cell.java
@@ -28,19 +28,28 @@ public class Cell {
 		return myCurrState;
 	}
 	
-	public int getState(int i){
-		return myCurrState.getState(i);
+	public int getState(String key){
+		return myCurrState.getStateAttrib(key);
 	}
-	public void setNextState(int val, int i){
-		if (myNextState == null)
-			myNextState = myCurrState.copy();
-		myNextState.setState(val, i);
+	public int getStateID(){
+		return myCurrState.getStateID();
 	}
 	
-	public void incrementState(int i){
+	public void setNextStateAttrib(int val){
 		if (myNextState == null)
-			myNextState = myCurrState.copy();
-		myNextState.increment(i);
+			myNextState = myCurrState.clone();
+		myNextState.setStateID(val);
+	}
+	public void setNextStateAttrib(int val, String key){
+		if (myNextState == null)
+			myNextState = myCurrState.clone();
+		myNextState.setStateAtttrib(val, key);
+	}
+	
+	public void incrementState(String key){
+		if (myNextState == null)
+			myNextState = myCurrState.clone();
+		myNextState.increment(key);
 	}
 	
 	public Point getLocation(){
