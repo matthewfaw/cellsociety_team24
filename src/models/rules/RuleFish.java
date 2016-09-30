add3bcb773872a0f6eb9f15d2ca4a3654d6fa12b
Weston Carvalho <wac12@duke.edu>
2016-09-29 21:51:42 -0400

    Remove magic strings from RuleFish. Deprecate the previously used RuleFish constructor.

--- src/models/rules/RuleFish.java
+++ src/models/rules/RuleFish.java
@@ -25,26 +25,56 @@ public class RuleFish extends Rule {
 	private int mySharkReproTime;
 	private int myFishEnergy;
 	
+	private final String chrononName;
+	private final String ageName;
+	private final String energyName;
+	
+	
 	
 	/**
 	 * @param fishReproTime time for fish to reproduce
 	 * @param sharkReproTime time for sharks to reproduce
 	 * @param fishEnergy energy sharks gain by eating a fish
+	 * @param attributeNames the names of the fish/shark attributes given in the xml [chrononName, ageName, energyName]
 	 */
+	public RuleFish(int fishReproTime, int sharkReproTime, int fishEnergy, String[] attributeNames) {
+		myFishReproTime = fishReproTime;
+		mySharkReproTime = sharkReproTime;
+		myFishEnergy = fishEnergy;
+		
+		chrononName = attributeNames[0];
+		ageName = attributeNames[1];
+		energyName = attributeNames[2];
+	}
+	
+	/**
+	 * Also pass a string array of the string names of each fish attribute. This method does the same thing as calling
+	 * RuleFish(fishReproTime, sharkReproTime, fishEnergy, {"Chronon", "Age", "Energy"})
+	 * 
+	 * @param fishReproTime time for fish to reproduce
+	 * @param sharkReproTime time for sharks to reproduce
+	 * @param fishEnergy energy sharks gain by eating a fish
+	 * @deprecated
+	 */
+	@Deprecated
 	public RuleFish(int fishReproTime, int sharkReproTime, int fishEnergy) {
 		myFishReproTime = fishReproTime;
 		mySharkReproTime = sharkReproTime;
 		myFishEnergy = fishEnergy;
+		
+		chrononName = "Chronon";
+		ageName = "Age";
+		energyName = "Energy";
 	}
 
 	@Override
 	public void calculateAndSetNextStates(GridModel grid) {
 		myGrid = grid;
 		
-		int prevChron = (int) myGrid.getCell(0, 0).getState("Chronon");
+		int prevChron = (int) myGrid.getCell(0, 0).getState(chrononName);
 		
 		for (Cell c: myGrid){
-			if ((int) c.getNextState("Chronon") <= prevChron){
+			if ((int) c.getNextState(chrononName) <= prevChron){
 				move(c);
 			}
 		}
@@ -57,29 +87,29 @@ public class RuleFish extends Rule {
 	private void move(Cell c){
 		//TODO: consolidate move statements
 		if (!occupied(c)){
-			c.incrementState("Chronon");
+			c.incrementState(chrononName);
 		} else {
 			if (c.getNextStateID() == fishID){
 				Cell nextMove = pickFishMove(c);
 				if (nextMove != null)
 					moveFish(c, nextMove);
 				else {
-					c.incrementState("Chronon");
-					c.incrementState("Age");
+					c.incrementState(chrononName);
+					c.incrementState(ageName);
 				}
 				
 			} else if ( c.getNextStateID() == sharkID){
-				if (c.getNextState("Energy") <= 0){
-					c.setNextState(newEmpty(c.getNextState("Chronon") + 1));
+				if (c.getNextState(energyName) <= 0){
+					c.setNextState(newEmpty(c.getNextState(chrononName) + 1));
 				} else {
-					c.decrement("Energy");
+					c.decrement(energyName);
 					Cell nextMove = pickSharkMove(c);
 					
 					if (nextMove != null)
 						moveShark(c, nextMove);
 					else {
-						c.incrementState("Chronon");
-						c.incrementState("Age");
+						c.incrementState(chrononName);
+						c.incrementState(ageName);
 					}
 				}
 			}
@@ -93,14 +123,14 @@ public class RuleFish extends Rule {
 	 */
 	private void moveFish(Cell c, Cell destination){
 		CellState fish = c.getNextState();
-		fish.increment("Chronon");
-		fish.increment("Age");
+		fish.increment(chrononName);
+		fish.increment(ageName);
 		
-		if (fish.getStateAttrib("Age") < myFishReproTime)
-			c.setNextState(newEmpty(fish.getStateAttrib("Chronon")));
+		if (fish.getStateAttrib(ageName) < myFishReproTime)
+			c.setNextState(newEmpty(fish.getStateAttrib(chrononName)));
 		else {
-			c.setNextState(newFish(fish.getStateAttrib("Chronon")));
-			fish.setStateAtttrib(0, "Age");
+			c.setNextState(newFish(fish.getStateAttrib(chrononName)));
+			fish.setStateAtttrib(0, ageName);
 		}
 		
 		destination.setNextState(fish);
@@ -113,18 +143,18 @@ public class RuleFish extends Rule {
 	 */
 	private void moveShark(Cell c, Cell destination){
 		CellState shark = c.getNextState();
-		shark.increment("Chronon");
-		shark.increment("Age");
+		shark.increment(chrononName);
+		shark.increment(ageName);
 		
-		if (shark.getStateAttrib("Age") < mySharkReproTime)
-			c.setNextState(newEmpty(shark.getStateAttrib("Chronon")));
+		if (shark.getStateAttrib(ageName) < mySharkReproTime)
+			c.setNextState(newEmpty(shark.getStateAttrib(chrononName)));
 		else {
-			c.setNextState(newShark(shark.getStateAttrib("Chronon"), shark.getStateAttrib("Energy")));
-			shark.setStateAtttrib(0, "Age");
+			c.setNextState(newShark(shark.getStateAttrib(chrononName), shark.getStateAttrib(energyName)));
+			shark.setStateAtttrib(0, ageName);
 		}
 		
 		if (destination.getNextStateID() == fishID)
-			shark.setStateAtttrib(shark.getStateAttrib("Energy") + myFishEnergy, "Energy");
+			shark.setStateAtttrib(shark.getStateAttrib(energyName) + myFishEnergy, energyName);
 		
 		destination.setNextState(shark);
 		
@@ -176,27 +206,27 @@ public class RuleFish extends Rule {
 
 	private CellState newEmpty(double chronon){
 		Map<String, Double> map = new TreeMap<String, Double>();
-		map.put("Chronon", chronon);
-		map.put("Age", 0.0);
-		map.put("Energy", 0.0);
+		map.put(chrononName, chronon);
+		map.put(ageName, 0.0);
+		map.put(energyName, 0.0);
 		
 		return new CellState(0, map);
 	}
 	
 	private CellState newFish(double chronon){
 		Map<String, Double> map = new TreeMap<String, Double>();
-		map.put("Chronon", chronon);
-		map.put("Age", 0.0);
-		map.put("Energy", 0.0);
+		map.put(chrononName, chronon);
+		map.put(ageName, 0.0);
+		map.put(energyName, 0.0);
 		
 		return new CellState(1, map);
 	}
 	
 	private CellState newShark(double chronon, double energy){
 		Map<String, Double> map = new TreeMap<String, Double>();
-		map.put("Chronon", chronon);
-		map.put("Age", 0.0);
-		map.put("Energy", energy);
+		map.put(chrononName, chronon);
+		map.put(ageName, 0.0);
+		map.put(energyName, energy);
 		
 		return new CellState(2, map);
 	}
