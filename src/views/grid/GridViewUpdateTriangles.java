a933ee9027330ebddce5d731c3254510f3a80e16
Guhan Muruganandam <gm114@duke.edu>
2016-09-30 17:23:21 -0400

    Powershell failure. Testing

--- src/views/grid/GridViewUpdateTriangles.java
+++ src/views/grid/GridViewUpdateTriangles.java
@@ -5,43 +5,42 @@ import java.util.Collection;
 
 import javafx.scene.Group;
 import javafx.scene.shape.Polygon;
-import javafx.scene.shape.Shape;
 import models.grid.Cell;
 import resources.AppResources;
 import views.styles.CellStyleGuide;
 
+/**
+ * A class used to build the Grid View with Triangular cells
+ * @author Guhan Muruganandam
+ *
+ */
+
 public class GridViewUpdateTriangles extends GridViewUpdate {
 
-	public GridViewUpdateTriangles(int width,int height,Dimension size,Group root,CellStyleGuide csg,Collection<Cell> cells) {
-		super(width,height,size,root,csg,cells);
+	public GridViewUpdateTriangles(int width,int height,Dimension dimensions,Group root,CellStyleGuide csg,Collection<Cell> cells) {
+		super(width,height,dimensions,root,csg,cells);
 	}
 
 	@Override
-	public void AddCell(int width, int height, Dimension dimensions, Cell currcell, Group root, CellStyleGuide csg) {
-		double cellwidth=(width*0.5)/(dimensions.getWidth());
-		double cellheight=(height*0.5)/(dimensions.getHeight());
-		int cellx=currcell.getLocation().getX();
-		int celly=currcell.getLocation().getY();
+	public void AddCell(Cell currcell) {
+		setCellLocation(currcell);
 		Polygon celltriangle=new Polygon();
-		if(((cellx+celly)%2)==0){
+		if(((myCellx+myCelly)%2)==0){
 			celltriangle.getPoints().addAll(new Double[]{
-				width*(AppResources.OFFSET)+(cellx*cellwidth*AppResources.HALF), height*(AppResources.OFFSET)+(cellheight*celly)+cellheight*AppResources.HALF,
-				width*(AppResources.OFFSET)+(cellx*cellwidth*AppResources.HALF)-cellwidth*AppResources.HALF, height*(AppResources.OFFSET)+(cellheight*celly)-cellheight*AppResources.HALF,
-				width*(AppResources.OFFSET)+(cellx*cellwidth*AppResources.HALF)+cellwidth*AppResources.HALF, height*(AppResources.OFFSET)+(cellheight*celly)-cellheight*AppResources.HALF	
+                xOffset+(myCellx*myCellWidth*AppResources.HALF), yOffset+(myCellHeight*myCelly)+myCellHeight*AppResources.HALF,
+                xOffset+(myCellx*myCellWidth*AppResources.HALF)-myCellWidth*AppResources.HALF, yOffset+(myCellHeight*myCelly)-myCellHeight*AppResources.HALF,
+                xOffset+(myCellx*myCellWidth*AppResources.HALF)+myCellWidth*AppResources.HALF, yOffset+(myCellHeight*myCelly)-myCellHeight*AppResources.HALF	
 			});
 		}
 		else{
 			celltriangle.getPoints().addAll(new Double[]{
-				width*(AppResources.OFFSET)+(cellx*cellwidth*AppResources.HALF), height*(AppResources.OFFSET)+(cellheight*celly)+cellheight*AppResources.HALF,
-				width*(AppResources.OFFSET)+(cellx*cellwidth*AppResources.HALF)-cellwidth*AppResources.HALF, height*(AppResources.OFFSET)+(cellheight*celly)-cellheight*AppResources.HALF,
-				width*(AppResources.OFFSET)+(cellx*cellwidth*AppResources.HALF)+cellwidth*AppResources.HALF, height*(AppResources.OFFSET)+(cellheight*celly)-cellheight*AppResources.HALF	
+				xOffset+(myCellx*myCellWidth*AppResources.HALF)-myCellWidth*AppResources.HALF, yOffset+(myCellHeight*myCelly)+myCellHeight*AppResources.HALF,
+				xOffset+(myCellx*myCellWidth*AppResources.HALF)+myCellWidth*AppResources.HALF, yOffset+(myCellHeight*myCelly)+myCellHeight*AppResources.HALF,
+				xOffset+(myCellx*myCellWidth*AppResources.HALF), yOffset+(myCellHeight*myCelly)-myCellHeight*AppResources.HALF	
 			});
 
 		}
-		cellMap.put(currcell, celltriangle);
-		colorCell(currcell,csg);
-		root.getChildren().add(celltriangle);
-        myShapeCollection.add(celltriangle); 
+		cellSetup(currcell,celltriangle);
 	}
 
 }
